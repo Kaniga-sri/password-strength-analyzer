9108dc9ac59b8fcf43345cb613c202fe27bd246c
Kaniga-sri <kanigasri81@gmail.com>
2026-06-08 10:04:18 +0530

    Update updated app.py

--- updated app.py
+++ updated app.py
@@ -1,21 +1,19 @@
 from flask import Flask, render_template, request
 from core import check_password
+import os
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-
     result = None
 
     if request.method == "POST":
         password = request.form["password"]
         result = check_password(password)
 
-    return render_template(
-        "index.html",
-        result=result
-    )
+    return render_template("index.html", result=result)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
